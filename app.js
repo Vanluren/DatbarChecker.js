997e1702be89e8cd3786e93e96639021e08d2e4d
Villads V K Nielsen <villadsvalur@gmail.com>
2016-09-08 11:37:42 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+//15.00 = 900 minutter
+
+
   var datbarCheck = function()
   {
       var text = '{"sentences":[' +
  '{"sentence":"sidde der hjemme og prikke øjnene ud over noget DADs"},' +
  '{"sentence":"sidde der hjemme og ikke forstå hvad der er op og ned i CompArk"},' +
  '{"sentence":"sidde der hjemme og Hade IntDes?"},' +
- '{"sentence":"sidde der hjemme og IKKE FÅ LAVET LEKTIER"},' +
- '{"sentence":""},' +
+ '{"sentence":"sidde der hjemme og regulere dine automater"},' +
+ '{"sentence":"sidde der hjemme og bede til Bouvins ånd om at forstå x86_64"},' +
  '{"sentence":""},' +
  '{"sentence":""},' +
  '{"sentence":""},' +
@@ -14,46 +17,47 @@
  '{"sentence":""}]}';
 
     var obj = JSON.parse(text);
-    var d = new Date();
-    var n = d.getDay();
-    var t = d.getTime();
-    var hours = d.getHours();
-    var minutes = d.getMinutes();
-    var weekday = ["Søndag", "Mandag" , "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", ];
+//Genopfind tiden
+    var todaysDate = new Date();
+    var todaysWeekday = todaysDate.getDay();
+    var currentTime = todaysDate.getTime();
+    var hour = todaysDate.getHours();
+    var minut = todaysDate.getMinutes();
+    var weekdayArr = ["Søndag", "Mandag" , "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+//Lær den at snakke
     var answer = document.getElementById("answer");
+//Lær den at finde rand tal
     var randomN = Math.round((Math.random()*10))
+//Debuging **** HUSK AT SLETTE DET HER FØR DEPLOY****
+    console.log(randomN);
+    console.log(obj.sentences.length);
 
-    if(randomN => obj.sentences.length){
-        console.log(obj.sentences.length);
-        console.log(randomN);
-    }
-
-    if (n == 5)
+    if (todaysWeekday == 5)
         {
-            if(hours == 02){
+            if(hour == 02){
                 answer.innerHTML = "Baren er desværre lukket nu!</br> <h3>(Så gå dog i seng.)</h3>"
             }
 
-            else if(hours < 15){
-                if(((hours - 1) * -1) === 1){
-                    answer.innerHTML="Baren åbner om " + ((hours - 15) * -1) + " Time! <h3>Vi ses der nede.</h3>";
+            else if(hour < 15){
+                if((hour - 1 ) <=1 ){
+                    answer.innerHTML="Baren åbner om " + ((hour - 14) * -1) + " Time og " + (60 - minut) + "minutter! <h3>Vi ses der nede.</h3>";
                 }
                 else {
-                    answer.innerHTML="Baren åbner om " + ((hours - 15) * -1) + " Timer! <h3>Vi ses der nede.</h3>";
+                    answer.innerHTML="Baren åbner om " + ((hour - 14) * -1) + " Timer og " + (60 - minut) + " minutter! <h3>Vi ses der nede.</h3>";
                 }
             }
             else{
-                if((hours - 15) === 0){
-                            answer.innerHTML = "Baren har været åben i " + minutes + " minutter. <h3>Så bare roligt, der er rigelig med tid før baren lukker.</h3>";
+                if((hour - 15) === 0){
+                            answer.innerHTML = "Baren har været åben i " + minut + " minutter. <h3>Så bare roligt, der er rigelig med tid før baren lukker.</h3>";
                         }
                         else {
-                            answer.innerHTML = "Baren har været åben i " + ((15 - hours) * -1) + " timer og " + minutes + " minutter";
+                            answer.innerHTML = "Baren har været åben i " + ((15 - hour) * -1) + " timer og " + minut + " minutter";
                         }
 
                 }
         }
       else
       {
-        answer.innerHTML = "Baren er desværre ikke åben, da det er " + weekday[n] + ". <h3>Burde du ikke også"+ " " + obj.sentences[randomN].sentence + "?" + "</h3>";
+        answer.innerHTML = "Baren er desværre ikke åben, da det er " + weekdayArr[todaysWeekday] + ". <h3>Burde du ikke også"+ " " + obj.sentences[randomN].sentence + "?" + "</h3>";
       }
   }
